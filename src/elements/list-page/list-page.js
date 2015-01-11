75be19695ce19314ed30e410c75034addadb923e
Aurélien Luciani <aurelien.luciani@gmail.com>
2015-01-11 21:48:50 +0100

    added libraries, factorized stylesheets, started mindmap-element

--- src/elements/list-page/list-page.js
+++ src/elements/list-page/list-page.js
@@ -4,6 +4,7 @@
   for (let i = 0; i < 25; i++) {
     list[i] = {
       name: `Mindmap n°${i + 1}`,
+      id: Math.random().toString(),
       date: {
         created: new Date(Date.now() - (Math.round(Math.random() * Math.pow(10, 10)))).toISOString(),
         changed: new Date(Date.now() - (Math.round(Math.random() * Math.pow(10, 8)))).toISOString()
